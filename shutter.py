88502c9d7650281f72a96645d14dec5259fcc0eb
Henk Uyttenhove <henk.uyttenhoveDEV@kawanda.Be>
2022-10-15 18:04:17 +0200

    Update shutter.py

--- shutter.py
+++ shutter.py
@@ -4,13 +4,14 @@ import time
 import subprocess
 
 
-print("programma is gestart")
+print("program has started")
+
+# The first line is required to detect when a new recording is started in Kstars/Ekos.  As os.system doesn't support feedback, it's only a trigger.
 while True:
     os.system("indi_eval -t 0 -w \'\"GPhoto CCD.CCD_EXPOSURE.CCD_EXPOSURE_VALUE\"\'>0.001")
 
-#subprocess.run(["indi_eval", " -t 0 -o \'\"GPhoto CCD.CCD_EXPOSURE.CCD_EXPOSURE_VALUE\"\'"])
-#print("trigger received", Value)
 
+# When a recording is started, we download all settings of Kstars/Indi, we load this into a list and filter out the actual exposure time
     result = subprocess.run('indi_getprop',stdout=subprocess.PIPE)
     resultString = list(result.stdout.decode("utf-8").split("\n"))
 
@@ -20,6 +21,7 @@ while True:
             ExposureTimer = int(float(matching[43:]))
             print(ExposureTimer)
 
+# we activate the relay so the camera is trigger (combine the focus/trigger cable)
     os.system("usbrelay nikon_1=1")
     time.sleep(ExposureTimer)
     os.system("usbrelay nikon_1=0")
